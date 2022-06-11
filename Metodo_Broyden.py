1a3878db9b04be8772adb4f749b32df98575ffb0
binmex <robin019@hotmail.com>
2022-06-11 13:18:13 -0500

    ya estan todos los metodos

--- Metodo_Broyden.py
+++ Metodo_Broyden.py
@@ -2,39 +2,43 @@
 from math import cos,sin,pi,exp,sqrt
 import numpy as np
 
+#se declaran las 3 funciones
 def Fs(x1,x2,x3):
     f1 = 3*x1-cos(x2*x3)-1/2
-    f2 = x1**2-81*(x2+0.1)**+sin(x3)+1.06
+    f2 = x1**2-81*(x2+0.1)**2+sin(x3)+1.06
     f3 = exp(-x1*x2)+20*x3+(10*pi-3)/3
     return np.matrix([[f1],[f2],[f3]]) #al momento de convertirla en una matriz sera una fila
 
+#hallamos el jacoviano y lo invertimos
 def Jinv(x1,x2,x3):
-    Ja = np.matrix([[3,x3*sin(x2*x3)],x2*sin(x2*x3)],[2*x1,-162*(x2+0.1),cos(x3)],[-x2*exp(-x1*x2),-x1*exp(-x3*x2)])
+    Ja = np.matrix([[3,x3*sin(x2*x3),x2*sin(x2*x3)],[2*x1,-162*(x2+0.1),cos(x3)],[-x2*exp(-x1*x2),-x1*exp(-x1*x2),20]])
     JI = np.linalg.inv(Ja)
     return JI
+#imprimimos archivo de cabecera
+print("k \t x1 \t\t x2 \t\t x3 \t\t||x(k)-x(k-1)||")
 
-print("k \t x1 \t x2 \t x3 \t ||x(k)-x(k-1)||")
-
-N = 10
-Xo = [0.1,0.1,-0.1]
+n = 10
+X0 = [0.1,0.1,-0.1]
 Tol = 0.000001
-a = Jinv(Xo[0],Xo[1],Xo[2])
-v = Fs(Xo[0],Xo[1],Xo[2])
+
+#seguir al pie de la letra
+a = Jinv(X0[0],X0[1],X0[2])
+v = Fs(X0[0],X0[1],X0[2])
 s = -a*v
-Xo = np.matrix(Xo).T+s
+X0 = np.matrix(X0).T+s  #.T es para transponer
 k = 2
 
-while k<N:
+while k<n:
     w = v
-    v = Fs(float([0][0]),float([1][0]),float([2],[0]))
+    v = Fs(float(X0[0][0]),float(X0[1][0]),float(X0[2][0]))
     y = v-w
     z = -a*y
     p = float(s.T*a*y)
     a = a+1/p*(s+z)*s.T*a
     s = -a*v
-    Xo = Xo+s
+    X0 = X0+s
     mag = sqrt(float(s[0][0])**2+float(s[1][0])**2+float(s[2][0])**2)
-    print("{0:1d} \t {1:1.6f] \t {3:1.6f} \t {4:1.6f}".format(k,float(Xo[0][0]),float(Xo[1][0]),float(Xo[2][0]),mag))
+    print("{0:1d} \t {1:1.6f} \t {2:1.6f} \t {3:1.6f} \t {4:1.6f}".format(k,float(X0[0][0]),float(X0[1][0]),float(X0[2][0]),mag))
     if mag < Tol:
         print("El procedimiento fue exitoso")
         break
--- Metodo_Muller.py
+++ Metodo_Muller.py
@@ -1,29 +1,41 @@
 from numpy import sign
 from numpy.lib.scimath import sqrt
+from tabulate import tabulate
 
-#metodo para hallar raices complejas siuuuuuuuuuuuuuuuuuu
+# metodo para hallar raices complejas siuuuuuuuuuuuuuuuuuu
+muller_table = []  # tabla de datos del metodo de biseccion
+raiz=[]  #donde se va a ir guardadnod las raices
 
-#Se ingresa la funcion
+# Se ingresa la funcion
 def funcion(x):
-    f = x**2 +2*x+5
+    f = x ** 2 + 2 * x + 5
     return f
 
+
 def muller(x0, x1, x2, tol):
-    error = 0.0001
+    error = 0.001
     x3 = 0
+    i=0
     while error > tol:
         c = funcion(x2)
-        b = ((x0 - x2)**2 * ((funcion(x1)) - (funcion(x2))) - (x1 - x2)**2*((funcion(x0)) - (funcion(x2)))) / ((x0 - x2) * (x1 - x2) * (x0 - x1))
-        a = ((x1 - x2) * (funcion(x0) - funcion(x2)) - (x0 - x2)*(funcion(x1) - funcion(x2))) / ((x0 - x2) * (x1 - x2) * (x0 - x1))
-        x3 = x2 - (2 * c) / (b + sign(b) * sqrt(b**2 - 4 * a * c))
+        b = ((x0 - x2) ** 2 * ((funcion(x1)) - (funcion(x2))) - (x1 - x2) ** 2 * ((funcion(x0)) - (funcion(x2)))) / (
+                    (x0 - x2) * (x1 - x2) * (x0 - x1))
+        a = ((x1 - x2) * (funcion(x0) - funcion(x2)) - (x0 - x2) * (funcion(x1) - funcion(x2))) / (
+                    (x0 - x2) * (x1 - x2) * (x0 - x1))
+        x3 = x2 - (2 * c) / (b + sign(b) * sqrt(b ** 2 - 4 * a * c))
         error = abs(x3 - x2)
         x0 = x1
         x1 = x2
         x2 = x3
+        raiz.append(x3)
+        muller_table.append(raiz[i])
+        i +=1
     return x3
-#===================================================================
+
+
+# ===================================================================
 # --------------------------Ingreso de datos------------------------
-#===================================================================
+# ===================================================================
 print('============================================================')
 print('------------------------Metodo Muller-----------------------')
 print('============================================================\n')
@@ -31,5 +43,6 @@ X0 = float(input('ingrese el X0: '))
 X1 = float(input('Ingrese el X1: '))
 X2 = float(input('ingrese el X2: '))
 Tol = float(input('Ingrese la tolerancia: '))
+print("========================================\nImprtante los valores en la tabla aproxima el 5 decimal\n========================================")
+print('La raiz es: ', muller(X0, X1, X2, Tol))
 
-print('La raiz es: ', muller(X0,X1,X2,Tol))
