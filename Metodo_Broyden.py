0235856d7b6e6c1863069afa64c221d5b7519595
binmex <robin019@hotmail.com>
2022-06-11 20:50:45 -0500

    metodos ya funcionales y con graficas

--- Metodo_Broyden.py
+++ Metodo_Broyden.py
@@ -1,46 +1,76 @@
 #Metodo de Broyden
 from math import cos,sin,pi,exp,sqrt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 #se declaran las 3 funciones
-def Fs(x1,x2,x3):
-    f1 = 3*x1-cos(x2*x3)-1/2
-    f2 = x1**2-81*(x2+0.1)**2+sin(x3)+1.06
-    f3 = exp(-x1*x2)+20*x3+(10*pi-3)/3
-    return np.matrix([[f1],[f2],[f3]]) #al momento de convertirla en una matriz sera una fila
+def Fs(x1,x2):
+    f1 = 4*x1**2-20*x1+1/4*x2**2+8
+    f2 = 1/2*x1*x2**2+2*x1-5*x2+8
+    return np.matrix([[f1],[f2]]) #al momento de convertirla en una matriz sera una fila
 
 #hallamos el jacoviano y lo invertimos
-def Jinv(x1,x2,x3):
-    Ja = np.matrix([[3,x3*sin(x2*x3),x2*sin(x2*x3)],[2*x1,-162*(x2+0.1),cos(x3)],[-x2*exp(-x1*x2),-x1*exp(-x1*x2),20]])
+def Jinv(x1,x2):
+    Ja = np.matrix([[8*x1-20,1/2*x2],[1/2*x2**2+2,x1*x2-5]])
     JI = np.linalg.inv(Ja)
     return JI
+
 #imprimimos archivo de cabecera
-print("k \t x1 \t\t x2 \t\t x3 \t\t||x(k)-x(k-1)||")
+print("k \t x1 \t\t x2 \t\t||x(k)-x(k-1)||")
 
 n = 10
-X0 = [0.1,0.1,-0.1]
-Tol = 0.000001
+X0 = [0,0]
+Tol = 0.1
 
 #seguir al pie de la letra
-a = Jinv(X0[0],X0[1],X0[2])
-v = Fs(X0[0],X0[1],X0[2])
+a = Jinv(X0[0],X0[1])
+v = Fs(X0[0],X0[1])
 s = -a*v
 X0 = np.matrix(X0).T+s  #.T es para transponer
 k = 2
-
+#print(v)
+#print(float(X0[0][0]))
+#print(float(X0[1][0]))
+print("=======================")
 while k<n:
     w = v
-    v = Fs(float(X0[0][0]),float(X0[1][0]),float(X0[2][0]))
+    v = Fs(float(X0[0][0]),float(X0[1][0]))
+    #print(v)
+    #print('valor de  A:',a)
     y = v-w
     z = -a*y
     p = float(s.T*a*y)
     a = a+1/p*(s+z)*s.T*a
     s = -a*v
     X0 = X0+s
-    mag = sqrt(float(s[0][0])**2+float(s[1][0])**2+float(s[2][0])**2)
-    print("{0:1d} \t {1:1.6f} \t {2:1.6f} \t {3:1.6f} \t {4:1.6f}".format(k,float(X0[0][0]),float(X0[1][0]),float(X0[2][0]),mag))
+    mag = sqrt(float(s[0][0])**2+float(s[1][0])**2)
+    print("{0:1d} \t {1:1.6f} \t {2:1.6f} \t {3:1.6f}".format(k,float(X0[0][0]),float(X0[1][0]),mag))
     if mag < Tol:
         print("El procedimiento fue exitoso")
         break
     k += 1
+#===========================================================
+#-------------------------Graficar--------------------------
 
+def funGraf(x,y):
+    f_x = 4*x**2-20*x+1/4*y**2+8
+    return f_x
+def funGraf2(x,y):
+    f_x = 1/2*x*y**2+2*x-5*y+8
+    return f_x
+fig = plt.figure()
+ax = Axes3D(fig)
+a = -10
+b = 10
+c = 50
+x = np.linspace(a,b,c)
+y = np.linspace(a,b,c)
+#graficar con superficio
+X , Y = np.meshgrid(x,y)
+Z,W = np.meshgrid(x,y)
+
+#ax.plot(x, y, funGraf(x,y))
+ax.plot_surface(X,Y,funGraf(X,Y))
+ax.plot_surface(X,Y,funGraf2(Z,W))
+plt.show()
--- Metodo_Muller.py
+++ Metodo_Muller.py
@@ -1,36 +1,15 @@
 from numpy import sign
 from numpy.lib.scimath import sqrt
+import matplotlib.pyplot as plt
+import numpy as np
 from tabulate import tabulate
 
-# metodo para hallar raices complejas siuuuuuuuuuuuuuuuuuu
 muller_table = []  # tabla de datos del metodo de biseccion
-raiz=[]  #donde se va a ir guardadnod las raices
 
-# Se ingresa la funcion
+# --------Se ingresa la funcion------
 def funcion(x):
-    f = x ** 2 + 2 * x + 5
+    f = x ** 2 + 4
     return f
-
-
-def muller(x0, x1, x2, tol):
-    error = 0.001
-    x3 = 0
-    i=0
-    while error > tol:
-        c = funcion(x2)
-        b = ((x0 - x2) ** 2 * ((funcion(x1)) - (funcion(x2))) - (x1 - x2) ** 2 * ((funcion(x0)) - (funcion(x2)))) / (
-                    (x0 - x2) * (x1 - x2) * (x0 - x1))
-        a = ((x1 - x2) * (funcion(x0) - funcion(x2)) - (x0 - x2) * (funcion(x1) - funcion(x2))) / (
-                    (x0 - x2) * (x1 - x2) * (x0 - x1))
-        x3 = x2 - (2 * c) / (b + sign(b) * sqrt(b ** 2 - 4 * a * c))
-        error = abs(x3 - x2)
-        x0 = x1
-        x1 = x2
-        x2 = x3
-        raiz.append(x3)
-        muller_table.append(raiz[i])
-        i +=1
-    return x3
 
 
 # ===================================================================
@@ -39,10 +18,55 @@ def muller(x0, x1, x2, tol):
 print('============================================================')
 print('------------------------Metodo Muller-----------------------')
 print('============================================================\n')
-X0 = float(input('ingrese el X0: '))
-X1 = float(input('Ingrese el X1: '))
-X2 = float(input('ingrese el X2: '))
-Tol = float(input('Ingrese la tolerancia: '))
-print("========================================\nImprtante los valores en la tabla aproxima el 5 decimal\n========================================")
-print('La raiz es: ', muller(X0, X1, X2, Tol))
+x0 = float(input('ingrese el X0: '))
+x1 = float(input('Ingrese el X1: '))
+x2 = float(input('ingrese el X2: '))
+tol = float(input('Ingrese la tolerancia: '))
 
+#ejecucion de la logica
+# def muller(x0, x1, x2, tol):
+error = 1
+x3 = 0
+i = 1
+
+while error > tol:
+    c = funcion(x2)
+    b = ((x0 - x2) ** 2 * ((funcion(x1)) - (funcion(x2))) - (x1 - x2) ** 2 * ((funcion(x0)) - (funcion(x2)))) / (
+            (x0 - x2) * (x1 - x2) * (x0 - x1))
+    a = ((x1 - x2) * (funcion(x0) - funcion(x2)) - (x0 - x2) * (funcion(x1) - funcion(x2))) / (
+            (x0 - x2) * (x1 - x2) * (x0 - x1))
+    x3 = x2 - (2 * c) / (b + sign(b) * sqrt(b ** 2 - 4 * a * c))
+    error = abs(x3 - x2) #obtencion de valor absoluto
+    x0 = x1
+    x1 = x2
+    x2 = x3
+    muller_table.append([i,x3,error]) #ingreso de datos a la tabla
+    #si el resultado es imaginario y desean saber el valor en cada iteracion descomentar la linea a continuacion
+    #print('iteracion ',i,'valor de x3: ',x3)
+    i += 1
+#return x3
+r = x3
+
+print("========================================\nImprtante los valores en la tabla aproxima el 5 decimal\n========================================")
+print(tabulate(muller_table, headers=["Iteracion", "X3", "ERR %"]))
+
+print('--------------------La raiz es: ',r,'--------------------')
+
+
+# ========================================================================================
+# ----------------------------------------Graficar----------------------------------------
+# ========================================================================================
+a = -5  # donde empieza el eje x
+b = 5  # donde termina el eje x
+n = 50  # la densidad de puntos para la grafica
+xn = np.linspace(a, b, n)  # Se generan los valores de x para construir la grafica
+yn = funcion(xn)
+plt.plot(xn, yn)  # frafica la funcion (establece taaño plano, traza la grafica)
+plt.grid(True)
+plt.axhline(0, color="#ff0000")  # establece las lineas de origen en x y el color
+plt.axvline(0, color="#ff0000")  # establece las lineas de origen en y y el color
+plt.plot(r, 0, 'ko', label=("Raiz Biseccion"))  # grafica el punto de corte x,y
+plt.title("Metodo Biseccion")
+plt.ylabel("Eje X")
+plt.xlabel("Eje y")
+plt.show()
