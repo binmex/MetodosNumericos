9080e13ccb0b817607bbd95dd5e8bbb888b60357
binmex <robin019@hotmail.com>
2022-10-31 14:33:38 -0500

    al metodo de Simpson 1tercio.py se le añadio la grafica

--- Simpson 1tercio.py
+++ Simpson 1tercio.py
@@ -1,5 +1,5 @@
 #FUNCION REGLA DE SIMPSON 1/3
-
+import matplotlib.pyplot as plt
 import numpy as np                        #importacion de librerias
 
 def integracion_sim13(x, y):              #Definicion de la funcion
@@ -22,7 +22,8 @@ def integracion_sim13(x, y):              #Definicion de la funcion
         else:
           sumy2 += y[i]                   #Sumatoria de pares
 
-      I = round((b - a)*(y[0] + 4*sumy1 + 2*sumy2 + y[n])/(3*n), 6) #Formula de la regla de simpson 1/3 multiple
+      I = round((b - a)*(y[0] + 4*sumy1 + 2*sumy2 + y[n])/(3*n), 6)  #Formula de la regla de simpson 1/3 multiple
+
 
     else:
       print("+")
@@ -48,3 +49,18 @@ y = 0.2 + 25*x - 200*x**2 + 675*x**3 - 900*x**4 + 400*x**5      #Funcion del pro
 #y = [5, 5.5, 5.7, 5.9, 6.3, 6.5, 6.9, 8, 9, 12, 15]
 
 print(integracion_sim13(x, y))               #solucion numerica de la integral
+
+#aqui empieza la graficacion de la funcion
+def funcion(xn):
+  f= 0.2 + 25*x - 200*x**2 + 675*x**3 - 900*x**4 + 400*x**5
+  return f
+
+yn = funcion(x)
+plt.plot(x, yn)  # frafica la funcion (establece taaño plano, traza la grafica)
+plt.grid(True)
+plt.axhline(0, color="#ff0000")  # establece las lineas de origen en x y el color
+plt.axvline(0, color="#ff0000")  # establece las lineas de origen en y y el color
+plt.title("Metodo simpson 1/3")
+plt.ylabel("Eje X")
+plt.xlabel("Eje y")
+plt.show()
